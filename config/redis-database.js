20d778f0ca054f5431fcc3f9db2c4a5093eed66f
Alejandro Rangel <arangel.glz@gmail.com>
2016-04-17 12:07:27 -0700

    added external redis server

--- config/redis-database.js
+++ config/redis-database.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || "development";
 var config = require("./config")[env];
 var redis = require('redis');
-var redisClient = redis.createClient(config.redisPort, config.redisAddress);
+var redisClient = redis.createClient(config.redis, {no_ready_check: true});
 
 redisClient.on('error', function (err) {
     console.log('Error ' + err);
