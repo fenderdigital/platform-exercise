6b9430a6ad4428b66161784719e8eee0568f07b5
Alejandro Rangel <arangel.glz@gmail.com>
2016-04-19 16:42:08 -0700

    Change config file

--- config/config.js
+++ config/config.js
@@ -8,18 +8,20 @@ var config = {
         "redis": "redis://h:p52h46aacr82889dp8h7eg1uoc@ec2-54-227-252-69.compute-1.amazonaws.com:17059"
     },
     "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "dialect": "postgres",
+        "url": "postgres://zufzdmqcjtvjno:b0iaIich2Ez1wbp7sGeplvFqFh@ec2-54-204-40-209.compute-1.amazonaws.com:5432/d3d971cqlhefct",
+        "secret": 'somethingsomethingdarkside',
+        "expiration": 600,
+        "saltRounds": 10,
+        "redis": "redis://h:p52h46aacr82889dp8h7eg1uoc@ec2-54-227-252-69.compute-1.amazonaws.com:17059"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "dialect": "postgres",
+        "url": "postgres://zufzdmqcjtvjno:b0iaIich2Ez1wbp7sGeplvFqFh@ec2-54-204-40-209.compute-1.amazonaws.com:5432/d3d971cqlhefct",
+        "secret": 'somethingsomethingdarkside',
+        "expiration": 600,
+        "saltRounds": 10,
+        "redis": "redis://h:p52h46aacr82889dp8h7eg1uoc@ec2-54-227-252-69.compute-1.amazonaws.com:17059"
     }
 };
 
