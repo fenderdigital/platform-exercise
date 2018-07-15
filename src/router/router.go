004051844c25e99d827cac07106a4006c8ef7f81
Ray <mrs.muneton@gmail.com>
2018-07-15 01:59:05 -0700

    token returned through api

--- src/router/router.go
+++ src/router/router.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/mrsmuneton/platform-test/src/token"
+	"github.com/mrsmuneton/platform-test/src/user"
 )
 
 func Routes() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/login", LoginHandler)
+	r.HandleFunc("/login", LoginHandler).Methods("POST")
 	r.HandleFunc("/logout", LogoutHandler)
 	r.HandleFunc("/user/{id:[0-9]+}", ShowUserHandler).Methods("GET")        //if READ right from jwt
 	r.HandleFunc("/user/{id:[0-9]+}", ShowUserHandler).Methods("POST")       //if WRITE right from jwt
@@ -21,7 +23,8 @@ func Routes() *mux.Router {
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Login Handler")
+	_, token := token.CreateUserJWT(user.User{CurrentPassword: "P0werpuff", Email: "cinnamon@nice.com", Name: "Ray May"})
+	w.Write([]byte(token))
 }
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Logout Handler")
