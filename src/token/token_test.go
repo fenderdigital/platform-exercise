2e415596c91c95d302539df50a2e78cdccb4d5b1
Ray <mrs.muneton@gmail.com>
2018-07-18 20:58:30 -0700

    fine

--- src/token/token_test.go
+++ src/token/token_test.go
@@ -1,27 +1,22 @@
 package token
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/mrsmuneton/platform-test/src/user"
 )
 
-// type token_string string
+type token_string string
 
 func getUserStub() user.User {
 	return user.User{CurrentPassword: "P0werpuff", Email: "cinnamon@nice.com", Name: "Ray May"}
 }
 
 func TestCreateJWTToReturnTokenString(t *testing.T) {
+	var e = Error{}
 	var userstub user.User = getUserStub()
-	var ts, e = CreateUserJWT(userstub)
-	fmt.Println(ts)
+	_, e = CreateUserJWT(userstub)
 	if e.Code != "" {
 		t.Error("Unexpected token generation error")
 	}
 }
-
-// func TestParseJWTToReturnUserReadRight(t *testing.T) {}
-//
-// func TestParseJWTToReturnUserReadWriteRight(t *testing.T) {}
